4e5880d1dd8235d8badedfb292402095842372d1
decoupled-qa-sandbox <118390008+decoupled-qa-sandbox@users.noreply.github.com>
2023-04-11 09:41:17 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -50,11 +50,13 @@ export default function Home({ menuItems, posts }) {
 }
 
 export async function getServerSideProps({ res }) {
+	console.log("headers here");
 	const { menuItems, menuItemHeaders } = await getFooterMenu();
 	const { posts, headers: postHeaders } = await getLatestPosts(12);
 
 	const headers = [menuItemHeaders, postHeaders];
 	setOutgoingHeaders({ headers, res });
+	console.log(headers);
 
 	return {
 		props: {
