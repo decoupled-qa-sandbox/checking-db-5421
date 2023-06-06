371db80d09b196bf98f270e97f7246877f4b051a
decoupled-qa-sandbox <118390008+decoupled-qa-sandbox@users.noreply.github.com>
2023-06-06 11:51:34 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,7 +16,7 @@ export default function Home({ menuItems, posts }) {
 					href="https://nextjs.org"
 				>
 					Next.js!
-				</a> multidev main 2 - pr
+				</a> multidev main 2 - pr - commit
 			</h1>
 			<div className="text-2xl">
 				<div className="bg-black text-white rounded flex items-center justify-center p-4">
